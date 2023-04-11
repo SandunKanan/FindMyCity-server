ac7504dc23280da5b9fa104208ce748cfb7bcc65
Sandun Kanangama <sandunkanangama@gmail.com>
2023-04-11 19:11:03 -0400

    Added readme, updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 
 module.exports = {
   client: 'mysql2',
   connection: {
     host: '127.0.0.1',
     user: 'root',
-    password: 'rootroot',
-    database: 'capstone_test',
+    password: process.env.PASSWORD,
+    database: 'capstone',
     charset: 'utf8',
   },
 };
