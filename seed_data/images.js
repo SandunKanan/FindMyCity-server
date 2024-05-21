df1d44c7212ae5c4dc73c9fe60b2a85d6eea844b
Sandun Kanangama <sandunkanangama@gmail.com>
2024-05-21 15:50:03 +0900

    Added updated image seeds file

--- seed_data/images.js
+++ seed_data/images.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 
-const image_seeds = JSON.parse(fs.readFileSync('./data/seed_data/image_seeds.json'))
+const image_seeds = JSON.parse(fs.readFileSync('./data/seed_data/updated_image_seeds.json'))
 
 module.exports = image_seeds;
