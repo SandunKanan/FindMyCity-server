e412dd7fa8ed1b20e83ec31c0725610111f634a9
Sandun Kanangama <sandunkanangama@gmail.com>
2024-05-22 14:12:56 +0900

    update Knexfile

--- knexfile.js
+++ knexfile.js
@@ -10,3 +10,17 @@ module.exports = {
     charset: 'utf8',
   },
 };
+
+// // knexfile.js
+// module.exports = {
+//   client: 'mysql2', // or 'mysql' for MySQL
+//   connection: {
+//     host: process.env.DB_HOST || '/cloudsql/YOUR_INSTANCE_CONNECTION_NAME',
+//     user: process.env.DB_USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DB_NAME,
+//   },
+//   migrations: {
+//     tableName: 'knex_migrations',
+//   },
+// };
